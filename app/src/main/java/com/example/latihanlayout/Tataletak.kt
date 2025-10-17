ff6a0f833a2367d3c1529d52afbc27fb127843ad
vararais <varadigmaisnann@gmail.com>
2025-10-17 15:32:53 +0700

    Membuat Fungsi TataLetakRow

--- app/src/main/java/com/example/latihanlayout/Tataletak.kt
+++ app/src/main/java/com/example/latihanlayout/Tataletak.kt
@@ -1,6 +1,11 @@
 package com.example.latihanlayout
 
+import android.inputmethodservice.Keyboard
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,6 +15,17 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun TataletakColumn(modifier: Modifier) {
     Column(modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
+
+@Composable
+fun TataletakRow(modifier: Modifier) {
+    Row(modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly) {
         Text(text = "Komponen1")
         Text(text = "Komponen2")
         Text(text = "Komponen3")
