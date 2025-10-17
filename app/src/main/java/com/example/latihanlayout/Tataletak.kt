650ee59c17042e93993ed8b7d7e6ed3de533579c
vararais <varadigmaisnann@gmail.com>
2025-10-17 15:52:11 +0700

    Membuat Fungsi TataletakRowColumn Kolom 2

--- app/src/main/java/com/example/latihanlayout/Tataletak.kt
+++ app/src/main/java/com/example/latihanlayout/Tataletak.kt
@@ -81,5 +81,11 @@ fun TataletakRowColumn(modifier: Modifier) {
             Text(text = "Komponen2Kolom1")
             Text(text = "Komponen3Kolom1")
         }
+        //Kolom2
+        Column {
+            Text(text = "Komponen1Kolom2")
+            Text(text = "Komponen2Kolom2")
+            Text(text = "Komponen3Kolom2")
+        }
     }
 }
